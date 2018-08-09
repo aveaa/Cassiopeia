f39cbdf0ef3e6ec9cd0a5783aa51bf853fc6cf50
cykable <36812758+cykable@users.noreply.github.com>
2018-08-09 09:09:31 +0300

    lol

--- bot.js
+++ bot.js
@@ -227,5 +227,5 @@ client.on('message', message => {
     });
 
     client.on('ready', () => {
-        client.user.setPresence({ game: { name: `Cosmic ⛧ Player's | ;help`, type: 1 } }).catch();
+        client.user.setPresence({ game: { name: `Cosmic ⛧ Player's | ;help`, type: 2 } }).catch();
     });
