c538e6f086798fa6d3c829230c5cb11060bc1c07
cykable <36812758+cykable@users.noreply.github.com>
2018-08-11 10:48:59 +0300

    савав

--- bot.js
+++ bot.js
@@ -98,8 +98,8 @@ if (['8ball'].includes(command)) {
 
 client.on('voiceStateUpdate', (old_, new_) => {
     let arr = {
-    '475282831220932623': '<@&476978677872328705>',
-    '475282664677703703': '<@&476978677872328705>'
+    '475282831220932623': '476978677872328705',
+    '475282664677703703': '476978677872328705'
     }
     if (old_.voiceChannel == new_.voiceChannel) return;
     if (old_.voiceChannel && arr[old_.voiceChannel.id]) new_.removeRole(arr[old_.voiceChannel.id]).catch();
