20df570c3837859c96a546035b56f396eb0b7bf4
cykable <36812758+cykable@users.noreply.github.com>
2018-08-17 18:02:16 +0300

    Delete tools.js

--- tools.js
+++
@@ -1,8 +0,0 @@
-module.exports.getGif = (category) => {
-    var gifs = require("./jsons/gifs.json");
-
-    var img = gifs[category]["gif"][this.randomInteger(0, gifs[category]["gif"].length - 1)];
-    var msg = gifs[category]["text"][this.randomInteger(0, gifs[category]["text"].length - 1)];
-
-    return { img, msg }
-};
