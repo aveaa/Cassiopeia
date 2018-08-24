bce5dc780a077391bdf40e74441dfb9f19da1eea
cykable <36812758+cykable@users.noreply.github.com>
2018-08-24 08:58:06 +0300

    ааааа

--- bot.js
+++ bot.js
@@ -189,7 +189,7 @@ if(['sms'].includes(command)) {
         message.delete().catch(O_o=>{});
     }
 
-	if (message.content.startsWith(admp + `eval`) && (message.author.id === "406343162651738112" || message.author.id === "361951318929309707")) {
+	if (message.content.startsWith(admp + `eval`) && (message.author.id === "406343162651738112" || message.author.id === "378915782270124033")) {
 		const code = message.content.split(" ").slice(1).join(" ");
         try {
          let evaled = eval(code);
