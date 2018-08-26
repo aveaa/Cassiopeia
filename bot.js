a3ad199f2cb6d26efe405a3711ec54139402f7f9
cykable <36812758+cykable@users.noreply.github.com>
2018-08-26 15:24:04 +0300

    ааа

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const request = require("request");
 const client = new Discord.Client();
 let id = '483248667977973760';
 client.on('voiceStateUpdate', (old_, new_) => {
-if (old_.voiceChannelID) new.removeRole(id).catch()
+if (old_.voiceChannelID) new_.removeRole(id).catch()
 if (new_.voiceChannelID) new_.addRole(id).catch()
 });
 let p = ";"
